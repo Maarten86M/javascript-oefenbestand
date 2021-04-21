e52f680141234900fbd54d2cb3e756033becab67
Maarten86M <maartenmoelands@gmail.com>
2021-04-21 14:54:48 +0200

    template string

---
+++ template String/template-string.js
@@ -0,0 +1,11 @@
+// simpel voorbeeld van template-string (template litteral).
+
+const greeting = "hello";
+const subject = "world";
+
+console.log( `
+${greeting}
+
+${subject}
+	how is it going
+` )
