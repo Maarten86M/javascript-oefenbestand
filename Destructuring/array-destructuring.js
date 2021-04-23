c61ab7d96ce024be0d011027b7fa9be0206806d0
Maarten86M <maartenmoelands@gmail.com>
2021-04-23 12:15:48 +0200

    voorbeelden array en object des. toegevoegd

--- Destructuring/array-destructuring.js
+++ Destructuring/array-destructuring.js
@@ -1,18 +1,43 @@
+//
+// const users = [
+//     {email: "rein@rein.com"},
+//     {email: "kelley@kelly.com"},
+//     {email: "miriam@me.com"}
+// ];
+// // tweede gebruiker
+// const kelly = users[1];
+// //derde gebruiker
+// const miriam = users[2];
+//
+// console.log(kelly);
+// console.log(miriam);
+//
+// // destructuring
+//
+// const [ rein, kelley, miriam] = users;
+// console.log(rein,kelley,miriam);
 
-const users = [
-    {email: "rein@rein.com"},
-    {email: "kelley@kelly.com"},
-    {email: "miriam@me.com"}
+
+// voorbeeld 2
+
+const schoolDetails = [
+    {
+        name: "henk",
+        age: 32,
+        num: 22
+    },
+    {
+        name: "Piet",
+        age: 18,
+        num: 21
+    },
+    {
+        name: "Jan",
+        age: 33,
+        num: 23
+    }
 ];
-// tweede gebruiker
-const kelly = users[1];
-//derde gebruiker
-const miriam = users[2];
 
-console.log(kelly);
-console.log(miriam);
+const [,, drie] = schoolDetails;
+console.log(drie);
 
-// destructuring
-
-const [ rein, kelley, miriam] = users;
-console.log(rein,kelley,miriam);
--- Destructuring/object-destructuring.js
+++ Destructuring/object-destructuring.js
@@ -1,11 +1,32 @@
-// leeg voorbeeld ter info.
-object spaceExLaunch = {
-    //hier staat alle info in.
+// // leeg voorbeeld ter info.
+// const spaceExLaunch = {
+//     //hier staat alle info in.
+// }
+//
+// const missionName = spaceExLaunch.mission_name;
+// const launchYear = spaceExLaunch.launch_year;
+// const rocketName = spaceExLaunch.rocket.rocket_name;
+//
+// // Maar dat kan doormiddel van destructuring natuurlijk veel korter.
+//     const { mission_name, launch_year, } = spaceExLaunch;
+
+
+    // VOORBEELD 2
+
+const objectName = {
+    objone: "appel",
+    objtwo: "peer",
+    objthree: "perzik",
+    objFour: "banaan",
+    objSix: ["fiets", "auto", "trekker", "vliegtuig", "skelter"],
+    objseven: {
+        huisnummer: 7,
+        woonplaats: "Amsterdam",
+        age: 32,
+    },
+    objnine: "taart",
+    objTen : "pizza"
 }
 
-const missionName = spaceExLaunch.mission_name;
-const launchYear = spaceExLaunch.launch_year;
-const rocketName = spaceExLaunch.rocket.rocket_name;
-
-// Maar dat kan doormiddel van destructuring natuurlijk veel korter.
-    const { mission_name, launch_year, } = spaceExLaunch;
+const { objone , objseven:{huisnummer,woonplaats} } = objectName;
+console.log(objone,huisnummer,woonplaats);
