4f25438dc6ab70c7489b51d38f65f569fe20011d
Maarten86M <maartenmoelands@gmail.com>
2021-04-23 09:06:17 +0200

    nieuwe voorbeelden .map toegevoegd

--- Array methoden (filter, find, map, sort)/map.js
+++ Array methoden (filter, find, map, sort)/map.js
@@ -17,5 +17,70 @@ for (let i = 0; i < students.length; i++){
 // Maar dit kan natuurlijk veel korter met de .map() methode.
 const outcome = students.map((student) => {
     return student + "je"; // er moet altijd een return in staan.
+}); // uitkomst na outcome loggen: [ 'Henkje', 'Pietje', 'Klaasje', 'fredje' ]
+
+// voorbeeld 2
+const studentDetails =  [
+    {
+        name: "Henk Jansen",
+        email: "henk@test.nl",
+        course: "FSD Bootcamp"
+    },
+    {
+        name: "Piet Pieters",
+        email: "piet@test.nl",
+        course: "HBO Sofware Dev"
+    },
+    {
+        name: "Marieke Smit",
+        email: "marieke@test.nl",
+        course: "FSD Bootcamp"
+    }
+];
+// oude manier:
+let emails = []; // lege array om de waarde in op te slaan binnen de for loop.
+for (let i = 0; i < studentDetails.length; i++){
+    emails.push(studentDetails[i].email);
+}
+// uitkomtst [ 'henk@test.nl', 'piet@test.nl', 'marieke@test.nl' ]
+
+// Ook dit kan korter!
+const emailAdress = studentDetails.map((studentdetail) => {
+    return studentdetail.email; // dit wordt in de nieuwe array gezet.
 });
-console.log(outcome);
+// uitkomst van emailAdress loggen: [ 'henk@test.nl', 'piet@test.nl', 'marieke@test.nl' ]
+
+// voorbeeld 3:
+
+const studentsName = ['Henk Jansen', 'Piet Pieters', 'Marieke Smit'];
+const prefixedStudents = studentsName.map((student) => {
+    return "leerling: " + student;
+}); // uitkomst na prefixed loggen: / ['Leerling: Henk Jansen', 'Leerling: Piet Pieters', 'Leerling: Marieke Smit']
+// dus we geven een naam mee die erbij gezet moet worden in de nieuwe array.
+// we slaan de waardes op in een nieuwe const. Om ze later weer te kunnen gebruiken.
+
+// voorbeeld 4
+const studentsCampusses = [
+    {
+        name: 'Henk Jansen',
+        course: 'FSD Bootcamp',
+    },
+    {
+        name: 'Piet Pieters',
+        course: 'HBO Software development',
+    },
+    {
+        name: 'Marieke Smit',
+        course: 'FSD Bootcamp',
+    },
+];
+
+const nameStudent = studentsCampusses.map((studentcampus) =>{
+    return "studentName:" + studentcampus.name;
+}) // uitkomst na loggen nameStudent:[ 'studentName:Henk Jansen', 'studentName:Piet Pieters', 'studentName:Marieke Smit']
+// we hebben nu de . notatie gebruikt om de specifieke namen te pakken.
+
+
+//
+// Voorbeelden uit de opdrachten van Github.
+//
