9ad39f4388e7bc3e1d63c5992e5ffd64c0ec0d6b
Maarten86M <maartenmoelands@gmail.com>
2021-04-29 15:28:32 +0200

    Random background function met event listeners

--- DOM (Document Object Model)/voorbeeld-project-29-04-2021/main.js
+++ DOM (Document Object Model)/voorbeeld-project-29-04-2021/main.js
@@ -1,21 +1,44 @@
-
 let counterTest = 0;
 
+// Button One
 const buttonTest = document.getElementById('button-test');
 buttonTest.addEventListener("click", () => {
     const header = document.getElementById('header-test');
     header.innerText = counterTest;
-    counterTest = counterTest +1;
+    counterTest = counterTest + 1;
 })
+
+// Button Two
 
 const buttonTestTwo = document.getElementById('button-test-two');
 buttonTestTwo.addEventListener("click", () => {
     // Nieuwe Paragraph toegevoegd wanneer er op de button wordt geklikt.
-    const addTekst = document.createElement('p')    ;
+    const addTekst = document.createElement('p');
     addTekst.innerText = "Je hebt geklikt";
     document.body.appendChild(addTekst);
     // Maar je kan de counter ook hergebruiken en doorlaten tellen met de tweede button
     const header = document.getElementById('header-test');
     header.innerText = counterTest;
-    counterTest = counterTest +1;
+    counterTest = counterTest + 1;
 })
+
+// Button Three
+
+// random background color function.
+
+function randomBG() {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
+    console.log(bgColor);
+    // random background color
+    document.body.style.background = bgColor;
+}
+
+// weergeven op de pagina.
+const buttonTestThree = document.getElementById('button-test-three');
+buttonTestThree.addEventListener("click", () => {
+    randomBG();
+})
+
